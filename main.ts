753e5f35389d0b27af9239ffd6d7a9d52759dae9
ThomasLeconte <thomasleconte05@gmail.com>
2022-05-22 17:37:57 +0200

    Fix: clean of main.ts file

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
 import { Bot } from './Bot';
 const config = require('./config.json');
 
-class Test {
-  toto: string
-  tata: string
-
-  constructor(){}
-}
-
 const client: Bot = new Bot(config["environments"][config.env]);
 console.log(`${client.name()} is starting on ${config.env} environment...`);
 
